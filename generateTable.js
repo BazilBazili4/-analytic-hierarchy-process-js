38e0ca9fff60f739ea4d44db372888dcac2e9533
kurlov-v <bazilevsvvv@gmail.com>
2019-12-01 16:12:06 +0300

    fixed input reflection

--- generateTable.js
+++ generateTable.js
@@ -1,4 +1,4 @@
-function createValueInput(className, inputId, row, col, defaultValue) {
+function createInput(className, inputId, row, col, defaultValue) {
     var input = document.createElement("input");
     input.setAttribute('class', className);
     input.setAttribute('id', inputId);
@@ -9,23 +9,32 @@ function createValueInput(className, inputId, row, col, defaultValue) {
     return input;
 }
 
+function createVoidInput() {
+    var input = document.createElement("input");
+    input.setAttribute('readonly', 'readonly');
+    return input;
+}
+
 function createTable(size) {
     var table = $('<table class="table"></table>'); //.addClass('foo');
     var sizeWithNodes = Number(size) + 1;
     for(var i = 0; i < sizeWithNodes; i++){
         var row = $('<tr></tr>');//.text('result ' + i);
         for(var j = 0; j < sizeWithNodes; j++){
-            var inputHtml = '';
+            var inputHtml = '';1
             if (i == 0 && j == 0) {
-                inputHtml = '<input type="text" ' + ' data-row='+ i + 'data-col=' + j + ' readonly>';
+                inputHtml = '';
+                // inputHtml = '<input type="text" ' + ' data-row='+ i + 'data-col=' + j + ' readonly>';
             } else if (i == 0 || j == 0) {
-                inputHtml = '<input type="text" ' + ' id="сriteria'+ i + '_' + j + '" class="сriteriaName"'  + 'data-row='+ i + ' data-col=' + j + ' value="Название">';
+                var inputId = 'criteriumName' + i + '_' + j; 
+                inputHtml = createInput('criteriumName', inputId, i, j, 'Название');
+                // inputHtml = '<input type="text" ' + ' id="сriteria'+ i + '_' + j + '" class="сriteriaName"'  + 'data-row='+ i + ' data-col=' + j + ' value="Название">';
             } else if (i == j) {
-                inputHtml = '<input type="number" ' + 'class="inputCriteria"'  + 'data-type="inputCriteria"' + ' value=1 readonly>';
+                inputHtml = '<input type="number" ' + 'class="criteriumValue"'  + 'data-type="inputCriteria"' + ' value=1 readonly>';
             } else {
-                var inputId = 'сriterium_' + i + '_' + j; 
-                inputHtml = createValueInput('criteriumValue', inputId, i, j);
-                inputHtml = '<input type="number" ' + 'class="inputCriteria"'  + 'data-type="inputCriteria"' + ' value=0>';
+                var inputId = 'criteriumValue' + i + '_' + j; 
+                inputHtml = createInput('criteriumValue', inputId, i, j, 0);
+                // inputHtml = '<input type="number" ' + 'class="inputCriteria"'  + 'data-type="inputCriteria"' + ' value=0>';
             }
 
             var element = $('<td></td>').append(inputHtml);//.text("result" + j + i); //append(input id ij)
@@ -44,7 +53,7 @@ function createCriterionTable(size, criterionName) {
         for(var j = 0; j < sizeWithNodes; j++){
             var inputHtml = '';
             if (i == 0 && j == 0) {
-                inputHtml = '<input type="text" ' + ' data-row='+ i + 'data-col=' + j + ' value="' + criterionName + '" readonly>';
+                inputHtml = '<input type="text"сriteriaName ' + ' data-row='+ i + 'data-col=' + j + ' value="' + criterionName + '" readonly>';
             } else if (i == 0 || j == 0) {
                 inputHtml = '<input type="text" ' + ' id="objects'+ i + '_' + j + '" class="сriteriaName"'  + 'data-row='+ i + ' data-col=' + j + ' value="Название">';
             } else if (i == j) {
@@ -61,12 +70,14 @@ function createCriterionTable(size, criterionName) {
     return table;
 }
 
-function reflectCriteriaName(inputCriteriaName, type) {
-    var reflectionCol = $(inputCriteriaName).data('row');
-    var reflectionRow = $(inputCriteriaName).data('col');
-    var reflectionId = '#' + type + reflectionRow + '_' + reflectionCol;
-    $(reflectionId).val($(inputCriteriaName).val());
+function reflectInputValue(input) {
+    var reflectionClass = $(input).attr('class');
+    var reflectionCol = $(input).data('row');
+    var reflectionRow = $(input).data('col');
+    var reflectionId = '#' + reflectionClass + reflectionRow + '_' + reflectionCol;
     console.log(reflectionId);
+
+    $(reflectionId).val($(input).val());
 }
 
 function showObjectControls() {
@@ -96,9 +107,9 @@ $(document).ready(function(){
         var countNodes = $("#countNodes").val();
         table.html(createTable(countNodes));
         showObjectControls();
-        field.on('change', '.сriteriaName', function (e) {
+        field.on('change', '.criteriumName', function (e) {
             console.log(e.target);
-            reflectCriteriaName(e.target, 'сriteria');
+            reflectInputValue(e.target);
         });
     });    
     // butCreateObjects.on('click', function () {
